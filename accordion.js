c4038174019602447fa461c4c2f2a297afc2782e
Andrey <asbogdanov13@gmail.com>
2022-01-14 12:25:13 +0500

    completed accordion

--- accordion.js
+++ accordion.js
@@ -1,17 +1,22 @@
 let acc = document.getElementsByClassName("accordion-title");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  
-  acc[i].addEventListener("click", () => {
-    // console.log(this.innerHTML)
+for (i = 0; i < acc.length; i++) { 
+  acc[i].addEventListener("click", function ()  {
     this.classList.toggle("active");
-    let panel = this.nextElementSibling;
+    const chevron = this.querySelector(".chevron");
+    const panel = this.nextElementSibling;
 
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
+    }
+
+    if (chevron.style.transform === "rotate(180deg)") {
+      chevron.style.transform = "none"
+    } else {
+      chevron.style.transform = "rotate(180deg)"
     }
   });
 }
