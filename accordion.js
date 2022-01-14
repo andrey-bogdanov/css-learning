5f7deca63b738f105931a01de1607d1f21666fdf
Andrey <asbogdanov13@gmail.com>
2022-01-14 16:49:40 +0500

    version 14 01 2022

--- accordion.js
+++ accordion.js
@@ -3,20 +3,6 @@ var i;
 
 for (i = 0; i < acc.length; i++) { 
   acc[i].addEventListener("click", function ()  {
-    this.classList.toggle("active");
-    const chevron = this.querySelector(".chevron");
-    const panel = this.nextElementSibling;
-
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-
-    if (chevron.style.transform === "rotate(180deg)") {
-      chevron.style.transform = "none"
-    } else {
-      chevron.style.transform = "rotate(180deg)"
-    }
+    this.parentNode.classList.toggle("active");
   });
 }
